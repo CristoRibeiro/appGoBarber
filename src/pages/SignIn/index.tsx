6845f34d322fb0b1962e42e0aa21ea007f2b475c
CristoRibeiro <cristianosrsilva@gmail.com>
2020-11-06 02:30:12 -0300

    AppPadraoReactTypeScript

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container } from './style';
+import { Container, Title } from './style';
 import Logo from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={Logo} />
+      <Title>Faça seu login!</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => console.log('deu')}>Entrar</Button>
     </Container>
   );
 };
